6807965dbdebf269e6e34900d9969dd8f25ced33
Andrew <feshykandrew.work@gmail.com>
2020-12-13 12:00:37 +0200

    header init

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
-
+    $('.header-menu').PopupSlider({
+        effect: 'right',
+        button: '.burger',
+        closeButton: '.burger',
+        container: 'header ',
+        onMenuOpen: function () {
+            document.activeElement.blur();
+        },
+        onMenuClose: function () {
+            document.activeElement.blur();
+        }
+    });
 })
