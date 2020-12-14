ab81786d2e2ac5e1d1e8f41ef3f4ecdbc335980c
Andrew <feshykandrew.work@gmail.com>
2020-12-14 20:19:48 +0200

    clients slider

--- js/main.js
+++ js/main.js
@@ -19,6 +19,49 @@ $(document).ready(function () {
         fade: false,
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrows: true
+        arrows: true,
+        swipeToSlide: true
+    });
+
+    $('.clients-slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: false,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: true,
+        swipeToSlide: true,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 })
